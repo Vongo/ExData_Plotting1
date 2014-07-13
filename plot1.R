03523d37cc888d84723e20e3ea70b986786fb977
Vongo <adelmarre@vongo.eu>
2014-07-13 19:49:28 +0200

    Cosmetics.

--- plot1.R
+++ plot1.R
@@ -4,7 +4,9 @@ data <- load.data()
 
 cat("Plotting plot1...\n")
 png(file="plot1.png",width=480,height=480)
+
 hist(data$Global_active_power, col = "red", ylab = "Frequency",
     xlab = "Global Active Power (kilowatts)", main = "Global Active Power ")
+
 dev.off()
 
